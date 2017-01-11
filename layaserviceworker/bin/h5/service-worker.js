1bf5b481b7c0fbce94a99b971f698482b6e45e47
OneWay <839357821@qq.com>
2017-01-11 15:06:41 +0800

    更新worker

--- layaserviceworker/bin/h5/service-worker.js
+++ layaserviceworker/bin/h5/service-worker.js
@@ -29,19 +29,37 @@ var CURRENT_CACHES = {
   font: 'font-cache-v' + CACHE_VERSION
 };
 
-self.addEventListener('activate', function(event) {
-	console.log('activate:');
+self.addEventListener('install', 
+function (event) {
+  console.log("install");
+  event.waitUntil(
+    fetch("./configs/file.json").then(
+      function (response) {
+      return response.json();
+    }
+    ).then(
+      function (data) {
+      console.log(data);
+    }).catch(
+      function (e) {
+      console.log("Oops, error");
+    });
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('activate:');
   // Delete all caches that aren't named in CURRENT_CACHES.
   // While there is only one cache in this example, the same logic will handle the case where
   // there are multiple versioned caches.
-  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
     return CURRENT_CACHES[key];
   });
 
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (expectedCacheNames.indexOf(cacheName) === -1) {
             // If this cache name isn't present in the array of "expected" cache names, then delete it.
             console.log('Deleting out of date cache:', cacheName);
@@ -53,12 +71,12 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   console.log('Handling fetch event for', event.request.url);
 
   event.respondWith(
-    caches.open(CURRENT_CACHES.font).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
+    caches.open(CURRENT_CACHES.font).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
         if (response) {
           // If there is an entry in the cache for event.request, then response will be defined
           // and we can just return it. Note that in this example, only font resources are cached.
@@ -75,13 +93,13 @@ self.addEventListener('fetch', function(event) {
         // We call .clone() on the request since we might use it in a call to cache.put() later on.
         // Both fetch() and cache.put() "consume" the request, so we need to make a copy.
         // (see https://fetch.spec.whatwg.org/#dom-request-clone)
-        return fetch(event.request.clone()).then(function(response) {
+        return fetch(event.request.clone()).then(function (response) {
           console.log('  Response for %s from network is: %O',
             event.request.url, response);
 
           if (response.status < 400 &&
-              response.headers.has('content-type') &&
-              response.headers.get('content-type').match(/^font\//i)) {
+            response.headers.has('content-type') &&
+            response.headers.get('content-type').match(/^font\//i)) {
             // This avoids caching responses that we know are errors (i.e. HTTP status code of 4xx or 5xx).
             // We also only want to cache responses that correspond to fonts,
             // i.e. have a Content-Type response header that starts with "font/".
@@ -102,7 +120,7 @@ self.addEventListener('fetch', function(event) {
           // Return the original response object, which will be used to fulfill the resource request.
           return response;
         });
-      }).catch(function(error) {
+      }).catch(function (error) {
         // This catch() will handle exceptions that arise from the match() or fetch() operations.
         // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
         // It will return a normal response object that has the appropriate error code set.
